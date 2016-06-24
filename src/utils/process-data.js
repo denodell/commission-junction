36abb48a46eaa961106ea9e898764dbf6c693332
Den Odell <den.odell@akqa.com>
2016-06-24 13:53:28 +0200

    Better support for Boolean values in data. Adding more tests.

--- src/utils/process-data.js
+++ src/utils/process-data.js
@@ -1,5 +1,6 @@
 export function normalizeAdvertiserData(merchants) {
   let pluralArrayValueFields = ['actions', 'linkTypes']
+  let booleanValueFields = ['mobileTrackingCertified', 'performanceIncentives']
   let pluralActionArrayValueFields = ['commission']
 
   return merchants.map(merchant => {
@@ -8,51 +9,52 @@ export function normalizeAdvertiserData(merchants) {
     for (let dataItem in merchant) {
       if (merchant.hasOwnProperty(dataItem)) {
         out[dataItem] = pluralArrayValueFields.includes(dataItem) ? merchant[dataItem][0][dataItem.replace(/s$/, '')] : merchant[dataItem][0]
+        out[dataItem] = booleanValueFields.includes(dataItem) ? out[dataItem] === 'true' : out[dataItem]
       }
     }
 
-		out.primaryCategory = {
-			parent: out.primaryCategory.parent[0],
-			child: out.primaryCategory.child[0],
-		}
+    out.primaryCategory = {
+      parent: out.primaryCategory.parent[0],
+      child: out.primaryCategory.child[0],
+    }
 
     out.actions = out.actions.map(action => {
-			let commission = action.commission[0]
-			let defaultCommission = commission.default[0]
+      let commission = action.commission[0]
+      let defaultCommission = commission.default[0]
 
-			if (defaultCommission._) {
-				commission.default = {
-					value: defaultCommission._,
-					type: defaultCommission.$.type,
-				}
-			} else {
-				commission.default = {
-					value: defaultCommission
-				}
-			}
+      if (defaultCommission._) {
+        commission.default = {
+          value: defaultCommission._,
+          type: defaultCommission.$.type,
+        }
+      } else {
+        commission.default = {
+          value: defaultCommission,
+        }
+      }
 
-			if (commission.itemlist) {
-				commission.itemlist = commission.itemlist.map(item => ({
-					value: item._,
-					name: item.$.name,
-					id: item.$.id,
-				}))
-			}
+      if (commission.itemlist) {
+        commission.itemlist = commission.itemlist.map(item => ({
+          value: item._,
+          name: item.$.name,
+          id: item.$.id,
+        }))
+      }
 
-			return {
-	      name: action.name[0],
-	      type: action.type[0],
-	      id: action.id[0],
-	      commission,
-			}
-		})
+      return {
+        name: action.name[0],
+        type: action.type[0],
+        id: action.id[0],
+        commission,
+      }
+    })
 
     return out
   })
 }
 
 export function normalizeLinkData(links) {
-	return links.map(link => {
+  return links.map(link => {
     let out = {}
     for (let linkItem in link) {
       if (link.hasOwnProperty(linkItem)) {
