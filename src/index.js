07f1a32f408fe1c41dc2f981ffa534af337f0344
Den Odell <den.odell@akqa.com>
2017-02-07 11:02:54 +0100

    Supporting import of transaction data by start/end date, defaulting to loading in the last year’s worth of data

--- src/index.js
+++ src/index.js
@@ -4,82 +4,84 @@ import { requestAdvertisers, requestLinks, requestProducts, requestTransactions,
 //import dateFormat from 'dateformat'
 
 const defaultOptions = {
-  developerKey: '',
-  websiteId: '',
+	developerKey: '',
+	websiteId: '',
 }
 
 export default class CommissionJunction {
-  constructor(options) {
-    this.options = Object.assign({}, defaultOptions, options)
-  }
+	constructor(options) {
+		this.options = Object.assign({}, defaultOptions, options)
+	}
 
-  getAdvertisers({
+	getAdvertisers({
     joined = undefined,
     advertiserIds = [],
     advertiserName,
     keywords,
     mobileTrackingCertified,
   } = {}) {
-    if (typeof joined !== 'undefined' || advertiserIds.length > 0) {
-      return requestAdvertisers({
-        joined,
-        advertiserIds,
-        developerKey: this.options.developerKey,
-      })
-    } else {
-      return Promise.all([
-        this.getAdvertisers({ joined: true }),
-        this.getAdvertisers({ joined: false }),
-      ])
+		if (typeof joined !== 'undefined' || advertiserIds.length > 0) {
+			return requestAdvertisers({
+				joined,
+				advertiserIds,
+				developerKey: this.options.developerKey,
+			})
+		} else {
+			return Promise.all([
+				this.getAdvertisers({ joined: true }),
+				this.getAdvertisers({ joined: false }),
+			])
       .then(resolvedPromises => resolvedPromises.reduce((previous, current) => previous.concat(current), []))
-    }
-  }
+		}
+	}
 
-  getLinks({
+	getLinks({
     joined = true,
   } = {}) {
-    return requestLinks({
-      joined,
-      websiteId: this.options.websiteId,
-      developerKey: this.options.developerKey,
-    })
-  }
+		return requestLinks({
+			joined,
+			websiteId: this.options.websiteId,
+			developerKey: this.options.developerKey,
+		})
+	}
 
 	getVouchers({
     joined = true,
   } = {}) {
-    return requestLinks({
-      joined,
-      websiteId: this.options.websiteId,
-      developerKey: this.options.developerKey,
+		return requestLinks({
+			joined,
+			websiteId: this.options.websiteId,
+			developerKey: this.options.developerKey,
 			vouchersOnly: true,
-    })
-  }
+		})
+	}
 
-  getTransactions() {
-    return requestTransactions({
-      websiteId: this.options.websiteId,
-      developerKey: this.options.developerKey,
-    })
-  }
+	getTransactions(startDate, endDate) {
+		return requestTransactions({
+			websiteId: this.options.websiteId,
+			developerKey: this.options.developerKey,
+			startDate,
+			endDate,
+		})
+	}
 
-  getTransactionItems({
+	getTransactionItems({
     originalActionIds = [],
   } = {}) {
-    return requestTransactionItems({
-      websiteId: this.options.websiteId,
-      developerKey: this.options.developerKey,
-      originalActionIds,
-    })
-  }
+		return requestTransactionItems({
+			websiteId: this.options.websiteId,
+			developerKey: this.options.developerKey,
+			originalActionIds,
+		})
+	}
 
   // TODO
-  getProducts() {
+	getProducts() {
 
-  }
+	}
 
   // TODO
-  searchProducts() {
+	searchProducts() {
 
-  }
+	}
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,62 +5,77 @@ const recordsPerPage = 100
 const startPageNumber = 1
 
 export function requestAdvertisers({ developerKey, joined }) {
-  let url = (joined) => `https://advertiser-lookup.api.cj.com/v3/advertiser-lookup?advertiser-ids=${joined ? 'joined': 'notjoined'}&records-per-page=${recordsPerPage}&page-number=${startPageNumber}`
+	let url = (joined) => `https://advertiser-lookup.api.cj.com/v3/advertiser-lookup?advertiser-ids=${joined ? 'joined': 'notjoined'}&records-per-page=${recordsPerPage}&page-number=${startPageNumber}`
 
-  return new Promise(async function(resolve, reject) {
-    try {
-      let advertisers = await requestData(url(joined), developerKey, 'advertisers', 'advertiser')
-      resolve(normalizeAdvertiserData(advertisers))
-    } catch (err) {
-      reject(err)
-    }
-  })
+	return new Promise(async function(resolve, reject) {
+		try {
+			let advertisers = await requestData(url(joined), developerKey, 'advertisers', 'advertiser')
+			resolve(normalizeAdvertiserData(advertisers))
+		} catch (err) {
+			reject(err)
+		}
+	})
 }
 
 export function requestLinks({ developerKey, websiteId, joined, vouchersOnly = false }) {
-  let url = (websiteId, joined) => `https://link-search.api.cj.com/v3/link-search?website-id=${websiteId}&advertiser-ids=${joined ? 'joined': 'notjoined'}&records-per-page=${recordsPerPage}&page-number=${startPageNumber}`
+	let url = (websiteId, joined) => `https://link-search.api.cj.com/v3/link-search?website-id=${websiteId}&advertiser-ids=${joined ? 'joined': 'notjoined'}&records-per-page=${recordsPerPage}&page-number=${startPageNumber}`
 
-  return new Promise(async function(resolve, reject) {
-    try {
-      let links = await requestData(url(websiteId, joined), developerKey, 'links', 'link')
+	return new Promise(async function(resolve, reject) {
+		try {
+			let links = await requestData(url(websiteId, joined), developerKey, 'links', 'link')
 			const output = normalizeLinkData(links)
-      resolve(vouchersOnly ? output.filter(link => !!link.couponCode) : output)
-    } catch (err) {
-      reject(err)
-    }
-  })
+			resolve(vouchersOnly ? output.filter(link => !!link.couponCode) : output)
+		} catch (err) {
+			reject(err)
+		}
+	})
 }
 
-export function requestTransactions({ developerKey, websiteId }) {
-  let url = `https://commission-detail.api.cj.com/v3/commissions?date-type=event&website-ids=${websiteId}`
+export async function requestTransactions({
+	developerKey,
+	websiteId,
+	startDate = new Date(Date.now() - (365 * 1000 * 60 * 60 * 24)), // 365 days ago
+	endDate = new Date(),	// now
+}) {
+	const dateRangeInDays = (endDate - startDate ) / (1000 * 60 * 60 * 24)
+	const numberOf30DayRanges = Math.ceil(dateRangeInDays / 30)
+	let index = 0
+	let promises = []
 
-  return new Promise(async function(resolve, reject) {
-    try {
-      let transactions = await requestData(url, developerKey, 'commissions', 'commission')
-      resolve(normalizeTransactionData(transactions))
-    } catch (err) {
-      reject(err)
-    }
-  })
+	for (; index < numberOf30DayRanges; index = index + 1) {
+		const startDateToUse = new Date(startDate.getTime() + (index * (30 * 1000 * 60 * 60 * 24)))
+		let endDateToUse = new Date(startDate.getTime() + ((index + 1) * (30 * 1000 * 60 * 60 * 24)))
+		if (endDateToUse > endDate) {
+			endDateToUse = endDate
+		}
+		const startDateISO = startDateToUse.toISOString().split('T')[0]
+		const endDateISO = endDateToUse.toISOString().split('T')[0]
+
+		const url = `https://commission-detail.api.cj.com/v3/commissions?date-type=event&website-ids=${websiteId}&start-date=${startDateISO}&end-date=${endDateISO}`
+		promises.push(requestData(url, developerKey, 'commissions', 'commission'))
+	}
+
+	const resultsData = await Promise.all(promises)
+	return resultsData.map(result => normalizeTransactionData(result)).reduce((prev, curr) => prev.concat(curr), [])
 }
 
 export function requestTransactionItems({ developerKey, websiteId, originalActionIds = [] }) {
-  let url = `https://commission-detail.api.cj.com/v3/item-detail/${originalActionIds.join(',')}`
+	let url = `https://commission-detail.api.cj.com/v3/item-detail/${originalActionIds.join(',')}`
 
-  if (originalActionIds.length === 0) {
-    return Promise.resolve([])
-  }
+	if (originalActionIds.length === 0) {
+		return Promise.resolve([])
+	}
 
-  return new Promise(async function(resolve, reject) {
-    try {
-      let transactionItems = await requestData(url, developerKey, 'itemDetails')
-      resolve(normalizeTransactionItemData(transactionItems))
-    } catch (err) {
-      reject(err)
-    }
-  })
+	return new Promise(async function(resolve, reject) {
+		try {
+			let transactionItems = await requestData(url, developerKey, 'itemDetails')
+			resolve(normalizeTransactionItemData(transactionItems))
+		} catch (err) {
+			reject(err)
+		}
+	})
 }
 
 export function requestProducts({ developerKey, joined }) {
-  let url = `https://product-search.api.cj.com/v2/product-search`
+	let url = `https://product-search.api.cj.com/v2/product-search`
 }
